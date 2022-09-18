78d540698213b727663408c97e3c226e193828eb
ramok <ramok@DESKTOP-FGH7VDS>
2022-09-18 19:29:42 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace favily.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
